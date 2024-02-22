ce5f98bf9fc4b845209d7d05fd1dd36b318bbabb
Dilan kanishka <dilankanishka032@gmail.com>
2024-02-22 11:00:20 +0530

    write update massge

--- resources/views/dilan/list.blade.php
+++ resources/views/dilan/list.blade.php
@@ -7,6 +7,23 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   </head>
   <body>
+
+    @if(session('msg'))
+      <div class="alert alert-success">
+          {{ session('msg') }}
+      </div>
+    @endif
+
+    {{-- @if (\session::has('msg'))
+      <div class="alert alert-success">
+        <ul>
+          <li>{|| \session::get('msg') ||}</li>
+        </ul>
+      </div>
+    @endif --}}
+  
+    {{-- this massege pass codee not work session word not work --}}
+
     <h1>Hello, world!</h1>
 
     <table class="table">
@@ -16,6 +33,7 @@
             <th scope="col">First Name</th>
             <th scope="col">address</th>
             <th scope="col">Tp</th>
+            <th> </th>
           </tr>
         </thead>
         <tbody>
@@ -25,6 +43,13 @@
                     <td>{{ $oneName->frist_name }}</td>
                     <td>{{ $oneName->address }}</td>
                     <td>{{ $oneName->tp }}</td>
+                    <td>
+                      <form action="{{url('dilan/'.$oneName->id)}}" method="post">
+                        @csrf
+                        @method('DELETE')
+                        <input type="submit" value="DELETE">
+                      </form>
+                    </td>
                 </tr>
             @endforeach
         </tbody>
